a44579174440c67577a8e59a16874c134c39ce24
Frédéric Bertolus <fred.bertolus@gmail.com>
2013-01-08 19:58:38 +0100

    Mine local position function

--- 3/fred/mine1.lua
+++ 3/fred/mine1.lua
@@ -1,3 +1,22 @@
-# Basic monolevel mining
+-- Basic monolevel mining
 
+function minePosition()
+	turtle.dig()
+	turtle.digUp()
+	turtle.digDown()
+	
+	-- Mine Left
+	turtle.turnLeft()
+	turtle.dig()
+	
+	-- Mine Right	
+	turtle.turnRight()
+	turtle.turnRight()
+	turtle.dig()
+	
+	turtle.turnLeft()
+		
+	print(turtle.getFuelLevel())
+end
 
+minePosition()
